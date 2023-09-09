945d192e851153b624364e0bb50accc0c9e19d87
DavesCodeData <136745107+DavesCodeData@users.noreply.github.com>
2023-09-09 10:59:37 -0400

    Add project files.

---
+++ ASP.NET-CORE-Model-View-Controller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CORE_Model_View_Controller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
